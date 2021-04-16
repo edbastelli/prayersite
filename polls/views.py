cb324cd39bf6f99a6bb48c452b3c5dfe562b7e0f
Ed Bastelli <ed@bastelli.net>
2021-04-15 21:28:17 -0400

    .gitignore, yourmom

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def yourmom(request):
+    return HttpResponse("Your mom. Hahaha")
 
 # Create your views here.
