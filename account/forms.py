9ba95732d3e9ea0a05f49ec1bb56f29a1f53c95e
Ed Bastelli <ed@bastelli.net>
2021-04-25 22:47:51 -0400

    Added built-in django.contrib.auth views

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,4 @@
 from django import forms
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+# class LoginForm(forms.Form):
+#     username = forms.CharField()
+#     password = forms.CharField(widget=forms.PasswordInput)
