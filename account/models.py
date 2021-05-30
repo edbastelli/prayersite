f9ca6032e903b83f333b04154581a003566b2f6b
Ed Bastelli <ed@bastelli.net>
2021-05-30 14:15:28 -0400

    profile following

--- account/models.py
+++ account/models.py
@@ -6,6 +6,10 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
+    following = models.ManyToManyField("self",
+                                symmetrical=False,
+                                blank=True,
+                                related_name='followers')
 
     def __str__(self):
         return f'Profile for user {self.user.username}'
